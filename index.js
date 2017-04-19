7b3912eba0897faba1eb858e1c16e8bee630e966
Manuel Resinas <resinas@us.es>
2017-04-19 05:30:12 +0000

    Added auth support

--- index.js
+++ index.js
@@ -4,6 +4,33 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var path = require('path');
 var contacts = require("./contacts.js");
+var users = require("./users.js");
+var passport = require('passport'),
+    BasicStrategy = require('passport-http').BasicStrategy,
+    LocalAPIKey = require('passport-localapikey').Strategy;
+    
+    
+passport.use(new BasicStrategy(
+    function(username, password, done) {
+        users.findOne({ username: username }, function (err, user) {
+          if (err) { return done(err); }
+          if (!user) { return done(null, false); }
+          if (!user.validPassword(password)) { return done(null, false); }
+          return done(null, user);
+        });
+        
+    }
+));
+
+passport.use(new LocalAPIKey(
+  function(apikey, done) {
+    users.findOne({ apikey: apikey }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      return done(null, user);
+    });
+  }    
+))
 
 var port = (process.env.PORT || 16778);
 var baseAPI = "/api/v1";
@@ -12,8 +39,9 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(passport.initialize());
 
-app.get(baseAPI + "/contacts", (request, response) => {
+app.get(baseAPI + "/contacts", passport.authenticate('localapikey', {session: false}), (request, response) => {
     console.log("GET /contacts"); 
     
     contacts.allContacts((err,contacts)=>{
@@ -88,8 +116,16 @@ contacts.connectDb((err) => {
         process.exit(1);
     }
     
+    users.connectDb((err) => {
+        if (err) {
+            console.log("Could not connect with MongoDB");
+            process.exit(1);
+        }
+        
+        app.listen(port, () => {
+            console.log("Server with GUI up and running!!");
+        });    
+    })
+    
 
-    app.listen(port, () => {
-        console.log("Server with GUI up and running!!");
-    });    
 });
